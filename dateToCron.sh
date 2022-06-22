2cf565996a74878710413a32ca0923c5d51db9c1
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-06-22 22:51:50 +0200

    Test version 1.1

--- dateToCron.sh
+++ dateToCron.sh
@@ -15,5 +15,6 @@ date >> /Users/kacperdworakowski/BlueSoft/date/data/data.txt
 # in croncmdpath you should add the path to the directory where you cloned repository
 # you can check the path of the directory by using pwd commend
 croncmdpath="/Users/kacperdworakowski/BlueSoft/date/"
+
 cronjob="* * * * * cd $croncmdpath && ./dateToCron.sh"
 ( crontab -l | grep -v -F "$croncmdpath" ; echo "$cronjob" ) | crontab -
