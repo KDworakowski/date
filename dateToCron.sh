5bc0eeddccc630e32815c8f3dc3ee5232354a8a5
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-06-22 22:26:52 +0200

    Test version 1.0

--- dateToCron.sh
+++ dateToCron.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+# DIR="./logs"
+# if ! [ -d "$DIR" ]; then
+#     mkdir logs
+# fi
+
+# DIR="./data"
+# if ! [ -d "$DIR" ]; then
+#     mkdir data
+# fi
+
+date >> ./data/data.txt
+
+# croncmdpath="Users/kacperdworakowski/BlueSoft/date/"
+# cronjob="* * * * * cd $croncmdpath && ./dateToCron.sh"
+# ( crontab -l | grep -v -F "$croncmdpath" ; echo "$cronjob" ) | crontab -
