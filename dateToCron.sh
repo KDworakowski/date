18f8169fb58bbcf2666f98e3411d31332a6f0688
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-06-22 22:59:10 +0200

    dateToCron Completed

--- dateToCron.sh
+++ dateToCron.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-DIR="./logs"
-if ! [ -d "$DIR" ]; then
-    mkdir logs
-fi
-
 DIR="./data"
 if ! [ -d "$DIR" ]; then
     mkdir data
 fi
 
-date >> /Users/kacperdworakowski/BlueSoft/date/data/data.txt
+# Here you should add the path to the directory where data.txt will be stored
+date >> /Users/.../date/data/data.txt
 
 # in croncmdpath you should add the path to the directory where you cloned repository
 # you can check the path of the directory by using pwd commend
-croncmdpath="/Users/kacperdworakowski/BlueSoft/date/"
+croncmdpath="/Users/.../date/"
 
 cronjob="* * * * * cd $croncmdpath && ./dateToCron.sh"
 ( crontab -l | grep -v -F "$croncmdpath" ; echo "$cronjob" ) | crontab -
