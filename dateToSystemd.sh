33b4f54345fae83e10561f339c6118675b1db2b2
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-06-23 16:50:30 +0200

    Systemd Test version 1.5

--- dateToSystemd.sh
+++ dateToSystemd.sh
@@ -13,10 +13,10 @@ SYSD_DATA_PATH="/home/kacper/date/systemd_data"
 
 if ! [[ -f "$SYSD_PATH/data_script.service" ]] && ! [[ -f "$SYSD_PATH/data_script.timer" ]]
 then
-    mv $SYSD_DATA_PATH/data_script.service $SYSD_PATH
-    mv $SYSD_DATA_PATH/data_script.timer $SYSD_PATH
-    systemctl daemon-reload
-    systemctl start data_script.timer
+    sudo mv $SYSD_DATA_PATH/data_script.service $SYSD_PATH
+    sudo mv $SYSD_DATA_PATH/data_script.timer $SYSD_PATH
+    sudo systemctl daemon-reload
+    sudo systemctl start data_script.timer
 fi
 
 # Here you should add the path to the directory where data.txt will be stored
